33b823d0464fa48e7abddf8e157eb45c7e12e6ab
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-06-21 16:25:37 -0400

    set up queries and added first components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
+import { getProductsInCollection } from "../lib/shopify";
+import ProductList from "../components/ProductList";
 
-export default function Home() {
+export default function Home({ products }) {
+  products = products.collectionByHandle.products.edges;
   return (
-    <div className='text-9xl'>
-      qwqwq
+    <div className="text-3xl">
+      <ProductList products={products} />
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  let products = await getProductsInCollection();
+  products = products.collectionByHandle.products.edges;
+  return {
+    props: {
+      products,
+    },
+  };
 }
