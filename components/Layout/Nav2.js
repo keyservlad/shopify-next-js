511876a2779be581d4bf67b3a2df5f2755625614
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-07-19 16:35:41 -0400

    bien avancé le header

--- components/Layout/Nav2.js
+++ components/Layout/Nav2.js
@@ -22,134 +22,74 @@ import {
   ShoppingBagIcon,
   XIcon,
 } from "@heroicons/react/outline";
+import Image from "next/image";
+import ImageSerieLim from "../../public/images/HP/header-serie.png";
+import ImageKrone from "../../public/images/HP/krone.png";
+import Link from "next/link";
 
 const navigation = {
   categories: [
     {
-      id: "women",
-      name: "Women",
+      id: "notre-cave",
+      name: "Notre Cave",
       featured: [
         {
-          name: "New Arrivals",
+          name: "Série limitée",
           href: "#",
           imageSrc:
             "https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg",
           imageAlt:
             "Models sitting back to back, wearing Basic Tee in black and bone.",
         },
-        {
-          name: "Basic Tees",
-          href: "#",
-          imageSrc:
-            "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
-          imageAlt:
-            "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
-        },
       ],
       sections: [
         {
-          id: "clothing",
-          name: "Clothing",
+          id: "en-ce-moment",
+          name: "En ce moment",
           items: [
-            { name: "Tops", href: "#" },
-            { name: "Dresses", href: "#" },
-            { name: "Pants", href: "#" },
-            { name: "Denim", href: "#" },
-            { name: "Sweaters", href: "#" },
-            { name: "T-Shirts", href: "#" },
-            { name: "Jackets", href: "#" },
-            { name: "Activewear", href: "#" },
-            { name: "Browse All", href: "#" },
+            { name: "Meilleures ventes", href: "#" },
+            { name: "Mieux notés", href: "#" },
+            { name: "Nos vins bio", href: "#" },
           ],
         },
         {
-          id: "accessories",
-          name: "Accessories",
+          id: "prix",
+          name: "Prix",
           items: [
-            { name: "Watches", href: "#" },
-            { name: "Wallets", href: "#" },
-            { name: "Bags", href: "#" },
-            { name: "Sunglasses", href: "#" },
-            { name: "Hats", href: "#" },
-            { name: "Belts", href: "#" },
+            { name: "Moins de 10 €", href: "#" },
+            { name: "Entre 10 et 20 €", href: "#" },
+            { name: "Entre 20 et 35 €", href: "#" },
+            { name: "Plus de 35 €", href: "#" },
           ],
         },
         {
-          id: "brands",
-          name: "Brands",
+          id: "couleur",
+          name: "Couleur",
           items: [
-            { name: "Full Nelson", href: "#" },
-            { name: "My Way", href: "#" },
-            { name: "Re-Arranged", href: "#" },
-            { name: "Counterfeit", href: "#" },
-            { name: "Significant Other", href: "#" },
-          ],
-        },
-      ],
-    },
-    {
-      id: "men",
-      name: "Men",
-      featured: [
-        {
-          name: "New Arrivals",
-          href: "#",
-          imageSrc:
-            "https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg",
-          imageAlt:
-            "Drawstring top with elastic loop closure and textured interior padding.",
-        },
-        {
-          name: "Artwork Tees",
-          href: "#",
-          imageSrc:
-            "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg",
-          imageAlt:
-            "Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.",
-        },
-      ],
-      sections: [
-        {
-          id: "clothing",
-          name: "Clothing",
-          items: [
-            { name: "Tops", href: "#" },
-            { name: "Pants", href: "#" },
-            { name: "Sweaters", href: "#" },
-            { name: "T-Shirts", href: "#" },
-            { name: "Jackets", href: "#" },
-            { name: "Activewear", href: "#" },
-            { name: "Browse All", href: "#" },
+            { name: "Blancs", href: "#" },
+            { name: "Rosés", href: "#" },
+            { name: "Rouges", href: "#" },
+            { name: "Effervescents", href: "#" },
           ],
         },
         {
-          id: "accessories",
-          name: "Accessories",
+          id: "region",
+          name: "Région",
           items: [
-            { name: "Watches", href: "#" },
-            { name: "Wallets", href: "#" },
-            { name: "Bags", href: "#" },
-            { name: "Sunglasses", href: "#" },
-            { name: "Hats", href: "#" },
-            { name: "Belts", href: "#" },
-          ],
-        },
-        {
-          id: "brands",
-          name: "Brands",
-          items: [
-            { name: "Re-Arranged", href: "#" },
-            { name: "Counterfeit", href: "#" },
-            { name: "Full Nelson", href: "#" },
-            { name: "My Way", href: "#" },
+            { name: "Bordeaux", href: "#" },
+            { name: "Bourgogne", href: "#" },
+            { name: "Vallée du Rhône", href: "#" },
+            { name: "Savoie", href: "#" },
+            { name: "Vallée de la Loire", href: "#" },
+            { name: "Languedoc-Roussillon", href: "#" },
           ],
         },
       ],
     },
   ],
   pages: [
-    { name: "Company", href: "#" },
-    { name: "Stores", href: "#" },
+    { name: "Bon Coup", href: "#" },
+    { name: "Le concept", href: "#" },
   ],
 };
 
@@ -161,7 +101,7 @@ export default function Nav2() {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="bg-white lg:sticky top-0 z-20">
+    <div className="bg-white lg:sticky top-0 z-20 border-b border-gray-200">
       {/* Mobile menu */}
       <Transition.Root show={open} as={Fragment}>
         <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
@@ -340,9 +280,9 @@ export default function Nav2() {
       <header className="relative bg-white">
         <nav
           aria-label="Top"
-          className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8"
+          className="w-full px-5 sm:px-10 lg:px-20 xl:px-28 2xl:px-40 mx-auto"
         >
-          <div className="border-b border-gray-200">
+          <div className="">
             <div className="h-16 flex items-center">
               <button
                 type="button"
@@ -402,41 +342,47 @@ export default function Nav2() {
                               />
 
                               <div className="relative bg-white">
-                                <div className="max-w-7xl mx-auto px-8">
-                                  <div className="grid grid-cols-2 gap-y-10 gap-x-8 py-16">
-                                    <div className="col-start-2 grid grid-cols-2 gap-x-8">
+                                <div className="w-full px-5 sm:px-10 xl:px-28 2xl:px-40 mx-auto">
+                                  <div className="grid grid-cols-6 gap-y-10 gap-x-8 py-16">
+                                    <div className="col-span-2">
                                       {category.featured.map((item) => (
                                         <div
                                           key={item.name}
-                                          className="group relative text-base sm:text-sm"
+                                          className="group relative text-base sm:text-sm w-full h-full aspect-[1.44]"
                                         >
-                                          <div className="aspect-w-1 aspect-h-1 rounded-lg bg-gray-100 overflow-hidden group-hover:opacity-75">
-                                            <img
-                                              src={item.imageSrc}
-                                              alt={item.imageAlt}
-                                              className="object-center object-cover"
+                                          <div className="rounded-lg overflow-hidden group-hover:opacity-75 group-hover:cursor-pointer relative w-full h-full">
+                                            <Image
+                                              src={ImageSerieLim}
+                                              alt="Image series lim"
+                                              layout="fill"
+                                              objectFit="contain"
+                                              quality={100}
+                                              placeholder="blur"
                                             />
+                                            <div className="absolute w-3/4 h-3/4 xl:px-10 border-2 z-30 left-0 right-0 top-0 bottom-0 m-auto border-[#DEC46F] text-center text-black flex flex-col justify-center items-center">
+                                              <div className="w-16">
+                                                <Image
+                                                  src={ImageKrone}
+                                                  alt="Image krone"
+                                                  placeholder="blur"
+                                                />
+                                              </div>
+                                              <h1 className="text-[#DEC46F] text-4xl mt-2">
+                                                Série limitée
+                                              </h1>
+                                              <p className="text-sm font-semibold mt-1">
+                                                Exclusivement réservée à nos
+                                                membres
+                                              </p>
+                                              <p className="underline text-xs font-semibold mt-3">
+                                                Je découvre
+                                              </p>
+                                            </div>
                                           </div>
-                                          <a
-                                            href={item.href}
-                                            className="mt-6 block font-medium text-gray-900"
-                                          >
-                                            <span
-                                              className="absolute z-10 inset-0"
-                                              aria-hidden="true"
-                                            />
-                                            {item.name}
-                                          </a>
-                                          <p
-                                            aria-hidden="true"
-                                            className="mt-1"
-                                          >
-                                            Shop now
-                                          </p>
                                         </div>
                                       ))}
                                     </div>
-                                    <div className="row-start-1 grid grid-cols-3 gap-y-10 gap-x-8 text-sm">
+                                    <div className="row-start-1 grid grid-cols-4 gap-y-10 gap-x-8 text-sm col-span-4">
                                       {category.sections.map((section) => (
                                         <div key={section.name}>
                                           <p
@@ -463,6 +409,14 @@ export default function Nav2() {
                                                 </a>
                                               </li>
                                             ))}
+                                            <li className="flex">
+                                              <a
+                                                href="#"
+                                                className="text-gray-900"
+                                              >
+                                                Voir tout
+                                              </a>
+                                            </li>
                                           </ul>
                                         </div>
                                       ))}
