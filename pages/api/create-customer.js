49a9e87ccc7a61498b1d3c0dc98c31fb0bd78302
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-08-04 10:11:02 -0400

    create account after buying a card

--- pages/api/create-customer.js
+++ pages/api/create-customer.js
@@ -1,27 +1,57 @@
-import { createCustomer } from "../../lib/shopifyCustomer";
+// called by webhook when order is paid
+// verify that a card is in the order
+// create the account with the field "status" toBeActivated
+// send mail to email adress to create the account
 
+import { createCustomer } from "../../lib/shopifyCustomerAdmin";
+
+// TODO add validation with the token in the header of the request
+// TODO set up email to my address with logs when there is an error
 export default async function send(req, res) {
-  // const passwordCreateUser = process.env.PASSWORD_CREATE_USER;
+  // req.body.line_items.map((item) => {
+  //   console.log(item);
+  //   if(item.title == "Carte Prestige") {
+  //     console.log("found it");
+  //     console.log("customer : " + req.body.customer.id + req.body.customer.email + req.body.customer.first_name + req.body.customer.last_name);
+  //   }
+  // })
 
-  // const customer = await createCustomer("guilhamat.arnaud@gmail.com", passwordCreateUser);
-  // console.log(req.oney);
-  // console.log(req.oney.customer);
-  // console.log(req.oney.line_items);
+  // console.log(
+  //   "line items : " +
+  //     JSON.stringify(req.body.line_items) +
+  //     "customer : " +
+  //     JSON.stringify(req.body.customer)
+  // );
 
-  req.body.line_items.map((item) => {
-    console.log(item);
-    if(item.title == "Carte Prestige") {
-      console.log("found it");
-      console.log("customer : " + req.body.customer.id + req.body.customer.email + req.body.customer.first_name + req.body.customer.last_name);
-    }
-  })
+  const customer = {
+    id: req.body.customer.id,
+    email: req.body.customer.email,
+  };
 
-  console.log(
-    "line items : " +
-      JSON.stringify(req.body.line_items) +
-      "customer : " +
-      JSON.stringify(req.body.customer)
-  );
+  const title = "prestige"
+
+  const input = {
+    email: customer.email,
+    id: customer.id,
+    metafields: [
+      {
+        id: "gid://shopify/Metafield/22028108824725",
+        key: "status",
+        namespace: "custom",
+        value: "toBeActivated",
+      },
+      {
+        id: "gid://shopify/Metafield/22028208078997",
+        key: "carte",
+        namespace: "custom",
+        value: title,
+      },
+    ],
+  };
+
+  const user = await createCustomer(JSON.stringify(input));
+  console.log(user);
+
   return res.status(200).json({ status: "Ok" });
 
   // // check for the POST request
