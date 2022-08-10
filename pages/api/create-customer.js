99ce755dcc08f38d85dfebe38171f86e783c0c9a
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-08-10 16:18:53 -0400

    forgot to parse

--- pages/api/create-customer.js
+++ pages/api/create-customer.js
@@ -65,7 +65,7 @@ export default async function send(req, res) {
 
   const input = attribute[0].value;
 
-  const meta = input.metafields;
+  const meta = JSON.parse(input).metafields;
   console.log("meta", meta);
   for (item in meta) {
     if (item.key.includes(boxBilling)) {
