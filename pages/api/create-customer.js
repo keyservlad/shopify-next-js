24cbd5b5b5142809578e0a92b18a27f6259ca9df
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-08-11 09:50:24 -0400

    print headers to verify webhook token

--- pages/api/create-customer.js
+++ pages/api/create-customer.js
@@ -14,73 +14,42 @@ import { sendMail } from "../../utils/sendMail";
 // TODO set up email to my address with logs when there is an error
 // TODO check que le membre n'a pas deja été créé car les webhooks sont appelé plusieurs fois souvent
 export default async function send(req, res) {
-  // req.body.line_items.map((item) => {
-  //   console.log(item);
-  //   if(item.title == "Carte Prestige") {
-  //     console.log("found it");
-  //     console.log("customer : " + req.body.customer.id + req.body.customer.email + req.body.customer.first_name + req.body.customer.last_name);
-  //   }
-  // })
 
-  // const customer = {
-  //   id: "gid://shopify/Customer/" + 6392490164373,
-  //   email: "guilhamat.arnaud@gmail.com",
-  // };
-  // const customer = {
-  //   id: "gid://shopify/Customer/" + req.body.customer.id,
-  //   email: req.body.customer.email,
-  // };
+  // check for the POST request
+  if (req.method !== "POST") {
+    return res
+      .status(400)
+      .json({ error: "Invalid HTTP method. Only POST requests are allowed." });
+  }
 
-  // const title = "prestige";
 
-  // const input = {
-  //   email: customer.email,
-  //   id: customer.id,
-  //   metafields: [
-  //     {
-  //       key: "status",
-  //       namespace: "custom",
-  //       value: "toBeActivated",
-  //     },
-  //     {
-  //       key: "carte",
-  //       namespace: "custom",
-  //       value: title,
-  //     },
-  //   ],
-  // };
-
-  // const user = await updateCustomer(JSON.stringify(input));
-  // console.log(user);
-  // const user2 = await createCustomer(customer.email, "test");
-  // console.log(user2);
+  req.body.line_items.map((item) => {
+    if(item.title == "Carte Prestige" || item.title == "Carte Découverte" || item.title == "Carte Immanquables") {
+      console.log("card trigger")
+    }
+  })
 
   // sendMail(customer.email, "subject", "<h1>html</h1>");
-  // console.log("full:", req.headers)
+  console.log("full:", req.headers)
+
+  // we get the order ID to get access to the custom attribute where the request is
   var headers = JSON.stringify(req.headers);
   headers = headers.replace("x-shopify-order-id", "orderId");
 
   const orderId = "gid://shopify/Order/" + JSON.parse(headers).orderId;
-
   var attribute = await getOrderCustomAttributes(orderId);
 
   var input = attribute[0].value;
-  input = input.replaceAll("~", '\\"');
-
-  console.log(input);
-
+  input = input.replaceAll("~", '\\"'); // formatting the request as it is stringified inside a parsed object
 
   // traiter les cas ou les deux adresses sont differentes
   // if userErrors not empty return 400
-  const customer = await updateCustomer(input);
-  console.log(customer);
+  var customer = await createCustomer(input);
+  console.log("create", customer);
+  customer = await updateCustomer(input);
+  console.log("update", customer);
 
   return res.status(200).json({ status: "Ok" });
 
-  // // check for the POST request
-  // if (req.method !== "POST") {
-  //   return res
-  //     .status(400)
-  //     .json({ error: "Invalid HTTP method. Only POST requests are allowed." });
-  // }
+  
 }
