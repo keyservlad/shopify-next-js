bfa656cd2289934672f771e896210d9f2d4784fe
Arnaud Guilhamat <guia2612@usherbrooke.ca>
2022-08-15 09:41:08 -0400

    user by email

--- pages/api/create-customer.js
+++ pages/api/create-customer.js
@@ -7,6 +7,7 @@ import {
   getOrderCustomAttributes,
   updateCustomer,
   createCustomer,
+  queryCustomerByEmail,
 } from "../../lib/shopifyCustomerAdmin";
 import { sendMail } from "../../utils/sendMail";
 
@@ -14,7 +15,6 @@ import { sendMail } from "../../utils/sendMail";
 // TODO set up email to my address with logs when there is an error
 // TODO check que le membre n'a pas deja été créé car les webhooks sont appelé plusieurs fois souvent
 export default async function send(req, res) {
-
   // check for the POST request
   if (req.method !== "POST") {
     return res
@@ -22,12 +22,15 @@ export default async function send(req, res) {
       .json({ error: "Invalid HTTP method. Only POST requests are allowed." });
   }
 
-
   req.body.line_items.map((item) => {
-    if(item.title == "Carte Prestige" || item.title == "Carte Découverte" || item.title == "Carte Immanquables") {
-      console.log("card trigger")
+    if (
+      item.title == "Carte Prestige" ||
+      item.title == "Carte Découverte" ||
+      item.title == "Carte Immanquables"
+    ) {
+      console.log("card trigger");
     }
-  })
+  });
 
   // sendMail(customer.email, "subject", "<h1>html</h1>");
 
@@ -43,12 +46,15 @@ export default async function send(req, res) {
 
   // traiter les cas ou les deux adresses sont differentes
   // if userErrors not empty return 400
+
+  console.log("attribute :", attribute);
+  console.log("input :", input);
   var customer = await createCustomer(input);
   console.log("create", customer);
+  var userByEmail = queryCustomerByEmail("email");
+  // get id and add it to input
   customer = await updateCustomer(input);
   console.log("update", customer);
 
   return res.status(200).json({ status: "Ok" });
-
-  
 }
