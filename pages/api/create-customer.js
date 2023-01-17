14f3937f61d0b460f6aa1907cada1932dc45a2b2
keyservlad <arnaud.guilhamat@isep.fr>
2023-01-17 23:50:24 +0100

    small change in the create customer to allow Plus Plus module

--- pages/api/create-customer.js
+++ pages/api/create-customer.js
@@ -39,14 +39,7 @@ export default async function send(req, res) {
     // res.send("ok"); // we have to return status 200 to avoid shopify from sending multiple webhooks requests and avoid multiple failures in a row resulting in deleting the webhook
     let isCard = false;
     req.body.line_items.map((item) => {
-      if (
-        item.title == "Carte Prestige" ||
-        item.title == "Carte Découverte" ||
-        item.title == "Carte Immanquables" ||
-        item.title == "Carte Prestige Domicile" ||
-        item.title == "Carte Découverte Domicile" ||
-        item.title == "Carte Immanquables Domicile"
-      ) {
+      if (item.title.includes("Carte")) {
         isCard = true;
       }
     });
